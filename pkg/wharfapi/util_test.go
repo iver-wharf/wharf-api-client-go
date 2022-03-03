b9964bf6260b0efec3a3a3b14f71fa73dd25843b
kalle (jag) <kalle.fagerberg@iver.se>
2022-03-03 15:51:46 +0100

    wharf-api gRPC client (#35)
    * Added protobuf def from wharf-api
    
    * Added gRPC implementation
    
    * Added auth
    
    * Updated godoc comment
    
    * Updated CHANGELOG.md
    
    * Added cutString tests
    
    * go mod tidy
    
    * Codacy ignore duplicates in generated code
    
    * Added alta/protopatch to set initialisms
    
    * Updated CHANGELOG.md
    
    * goimports -w
    
    * Changed gRPC to not stream chunks

--- pkg/wharfapi/util_test.go
+++ pkg/wharfapi/util_test.go
@@ -50,3 +50,73 @@ func TestFirstRuneLower(t *testing.T) {
 		})
 	}
 }
+
+func TestCutString_ok(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want1 string
+		want2 string
+	}{
+		{
+			name:  "Cuts at middle",
+			input: "foo bar",
+			want1: "foo",
+			want2: "bar",
+		},
+		{
+			name:  "Cuts at start",
+			input: " bar",
+			want1: "",
+			want2: "bar",
+		},
+		{
+			name:  "Cuts at end",
+			input: "foo ",
+			want1: "foo",
+			want2: "",
+		},
+		{
+			name:  "Cuts only first",
+			input: "foo bar moo",
+			want1: "foo",
+			want2: "bar moo",
+		},
+		{
+			name:  "Empty results",
+			input: " ",
+			want1: "",
+			want2: "",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got1, got2, ok := cutString(test.input, ' ')
+			assert.True(t, ok, "cut string OK?")
+			assert.Equal(t, test.want1, got1, "return value 1")
+			assert.Equal(t, test.want2, got2, "return value 2")
+		})
+	}
+}
+
+func TestCutString_notOk(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+	}{
+		{
+			name:  "Empty string",
+			input: "",
+		},
+		{
+			name:  "No delimiter",
+			input: "foobar",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			_, _, ok := cutString(test.input, ' ')
+			assert.False(t, ok, "cut string not OK?")
+		})
+	}
+}
